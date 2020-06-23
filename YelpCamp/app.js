af98066be03635a013d1b09bb1d8ae1a2076d528
Malte <malte.neuendorf@yahoo.de>
2020-06-24 00:02:03 +0200

    campgroundspage routes added, navbar added, styles added

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Granite Hill", image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Salmon Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Granite Hill", image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350"}
+]
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-    let campgrounds = [
-        {name: "Salmon Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"},
-        {name: "Granite Hill", image: "https://pixabay.com/get/55e8dc404f5aab14f1dc84609620367d1c3ed9e04e50744077287dd79145cd_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350"}
-    ]
-
     res.render("campgrounds", {campgrounds: campgrounds});
+})
+
+app.post("/campgrounds", (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampgrounds = {name:name, image:image}
+    campgrounds.push(newCampgrounds);
+
+    res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new")
 })
 
 app.listen(3000, () => {
