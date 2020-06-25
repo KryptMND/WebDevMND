7534c37735817dc555dc32545fc921d2246238a9
Malte <malte.neuendorf@yahoo.de>
2020-06-25 13:03:14 +0200

    YelpCamp connected with DB

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,39 +1,71 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+    
+//     {
+//         name: "Granite Hill", 
+//         image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&h=350"
+
+//     }, (err, campground) => {
+//         if (err){
+//             console.log(err);
+//         } else {
+//             console.log("New Campground added to Database!");
+//             console.log(campground);
+//         }
+//     });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Granite Hill", image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Salmon Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Granite Hill", image: "https://images.pexels.com/photos/776117/pexels-photo-776117.jpeg?auto=compress&cs=tinysrgb&h=350"},
-    {name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/803226/pexels-photo-803226.jpeg?auto=compress&cs=tinysrgb&h=350"}
-]
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
-
+// INDEX - show all campgrounds
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", {campgrounds: campgrounds});
-})
-
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    })
+});
+// CREATE - create new campgrounds
 app.post("/campgrounds", (req, res) => {
+    // Get data from form and save to campgrounds array
     let name = req.body.name;
     let image = req.body.image;
     let newCampgrounds = {name:name, image:image}
-    campgrounds.push(newCampgrounds);
-
-    res.redirect("/campgrounds");
-})
-
+    // Create a new Campground and save to DB
+    Campground.create(newCampgrounds, (err, newlyCreated) => {
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
+});
+// NEW - show form to create new campground 
 app.get("/campgrounds/new", (req, res) => {
     res.render("new")
-})
+});
 
 app.listen(3000, () => {
     console.log("The Yelp Camp Server has Started!");
